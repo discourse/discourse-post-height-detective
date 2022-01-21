0a1c881d234ee871a375cc43e09bcb7694ad8efe
Jarek Radosz <jradosz@gmail.com>
2022-01-21 18:52:22 +0100

    FEATURE: Show more debug info about the post

--- javascripts/discourse/initializers/post-height-detective.js
+++ javascripts/discourse/initializers/post-height-detective.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 import { withPluginApi } from "discourse/lib/plugin-api";
 import { bind } from "discourse-common/utils/decorators";
 import { cancel, later } from "@ember/runloop";
@@ -9,6 +11,12 @@ export default {
     withPluginApi("1.1.0", (api) => {
       api.modifyClass("component:scrolling-post-stream", {
         pluginId: "post-height-detective",
+
+        init() {
+          this._super(...arguments);
+          this.recordedHeights = {};
+          this.recordedHtml = {};
+        },
 
         afterRender() {
           // Do this super quick whenever rerendering happens,
@@ -47,32 +55,39 @@ export default {
 
         _refresh() {
           this._super(...arguments);
-          this.set("recordedHeights", {});
+          this.recordedHeights = {};
+          this.recordedHtml = {};
         },
 
         @bind
         trackHeight(post) {
-          const recordedHeights = this.recordedHeights || {};
           const postId = post.id.split("_")[1];
           const topicId = this.get("posts.posts.firstObject.topic.id");
           const postLink = `${window.location.protocol}//${window.location.hostname}/t/${topicId}/${postId}`;
-          const renderedHeight = post
-            .querySelector(".cooked")
-            .getBoundingClientRect().height;
-          const oldHeight = recordedHeights[postId];
+          const postElement = post.querySelector(".cooked");
+          const renderedHeight = postElement.getBoundingClientRect().height;
+          const oldHeight = this.recordedHeights[postId];
 
           if (oldHeight && renderedHeight !== oldHeight) {
-            // eslint-disable-next-line no-console
             console.error(
               `🕵 Height of ${postLink} has changed after initial render. Was ${oldHeight}, now ${renderedHeight}`
             );
+
+            if (this.recordedHtml[postId] === postElement.outerHTML) {
+              console.log("No HTML change, check CSS");
+            } else {
+              console.log(
+                `Previous HTML:\n\n${this.recordedHtml[postId]}\n\nCurrent HTML:\n\n${postElement.outerHTML}`
+              );
+            }
+
             document
               .querySelector("body")
               .classList.add("suspicious-post-detected");
           }
 
-          recordedHeights[postId] = renderedHeight;
-          this.set("recordedHeights", recordedHeights);
+          this.recordedHeights[postId] = renderedHeight;
+          this.recordedHtml[postId] = postElement.outerHTML;
         },
       });
 
