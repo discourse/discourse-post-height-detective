ed9a19d3d88d2fdda5776bcc3e543fc4f95748c5
David Taylor <david@taylorhq.com>
2022-11-01 11:33:20 +0000

    Modernise implementation to use ResizeObserver and WeakMap
    This should make the detection far more reliable because it's no longer depending on the intricacies of the widget rendering process and the Ember runloop. Instead, we trust the browser to inform us when the size of a painted element changes. This also allows us to remove the 'tick'-based height calculation.
    
    Plain objects are replaced with element-keyed WeakMaps to avoid the need for manual cleanup (and to fix issues which could happen when navigating directly between two topics with the loading slider enabled).
    
    `next()` is used when adding the body class to avoid a race condition with `api.onPageChange` which would break the red header on an initial page load.

--- javascripts/discourse/initializers/post-height-detective.js
+++ javascripts/discourse/initializers/post-height-detective.js
@@ -2,7 +2,7 @@
 
 import { withPluginApi } from "discourse/lib/plugin-api";
 import { bind } from "discourse-common/utils/decorators";
-import { cancel, later } from "@ember/runloop";
+import { next } from "@ember/runloop";
 
 export default {
   name: "post-height-detective",
@@ -14,80 +14,61 @@ export default {
 
         init() {
           this._super(...arguments);
-          this.recordedHeights = {};
-          this.recordedHtml = {};
+          this.recordedHeights = new WeakMap();
+          this.recordedHtml = new WeakMap();
+          this.resizeObserver = new ResizeObserver(this.resizeObserverCallback);
         },
 
         afterRender() {
           // Do this super quick whenever rerendering happens,
           // otherwise we might miss something
-          this.checkForHeightChanges();
+          this.trackElements();
         },
 
-        checkForHeightChanges() {
+        trackElements() {
           this.element
             .querySelectorAll("article[data-post-id]")
-            .forEach(this.trackHeight);
-        },
-
-        tick() {
-          const nextTick = later(
-            this,
-            () => {
-              this.checkForHeightChanges();
-              this.tick();
-            },
-            1000
-          );
-          this.set("nextTick", nextTick);
-        },
-
-        didInsertElement() {
-          this._super(...arguments);
-          this.tick();
-        },
-
-        willDestroyElement() {
-          this._super(...arguments);
-          const nextTick = this.nextTick;
-          cancel(nextTick);
-        },
-
-        _refresh() {
-          this._super(...arguments);
-          this.recordedHeights = {};
-          this.recordedHtml = {};
+            .forEach((e) => this.resizeObserver.observe(e));
         },
 
         @bind
-        trackHeight(post) {
-          const postId = post.id.split("_")[1];
+        resizeObserverCallback(resizeObserverEntries) {
+          for (const entry of resizeObserverEntries) {
+            this.trackHeight(entry.target, entry.contentRect.height);
+          }
+        },
+
+        @bind
+        trackHeight(post, renderedHeight) {
+          const postNumber = post.id.split("_")[1];
           const topicId = this.get("posts.posts.firstObject.topic.id");
-          const postLink = `${window.location.protocol}//${window.location.hostname}/t/${topicId}/${postId}`;
+          const postLink = `${window.location.protocol}//${window.location.hostname}/t/${topicId}/${postNumber}`;
           const postElement = post.querySelector(".cooked");
-          const renderedHeight = postElement.getBoundingClientRect().height;
-          const oldHeight = this.recordedHeights[postId];
+          const oldHeight = this.recordedHeights.get(post);
 
           if (oldHeight && renderedHeight !== oldHeight) {
             console.error(
               `🕵 Height of ${postLink} has changed after initial render. Was ${oldHeight}, now ${renderedHeight}`
             );
 
-            if (this.recordedHtml[postId] === postElement.outerHTML) {
+            const recordedHtml = this.recordedHtml.get(post);
+            if (recordedHtml === postElement.outerHTML) {
               console.log("No HTML change, check CSS");
             } else {
               console.log(
-                `Previous HTML:\n\n${this.recordedHtml[postId]}\n\nCurrent HTML:\n\n${postElement.outerHTML}`
+                `Previous HTML:\n\n${recordedHtml}\n\nCurrent HTML:\n\n${postElement.outerHTML}`
               );
             }
 
-            document
-              .querySelector("body")
-              .classList.add("suspicious-post-detected");
+            next(() =>
+              document
+                .querySelector("body")
+                .classList.add("suspicious-post-detected")
+            );
           }
 
-          this.recordedHeights[postId] = renderedHeight;
-          this.recordedHtml[postId] = postElement.outerHTML;
+          this.recordedHeights.set(post, renderedHeight);
+          this.recordedHtml.set(post, postElement.outerHTML);
         },
       });
 
